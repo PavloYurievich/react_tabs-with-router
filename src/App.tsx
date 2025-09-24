8c62f30eaeb6f8826d7de1826e3706c8b71fa6f5
PavloYurievich <pavelyurevich777@gmail.com>
2025-09-24 18:26:32 +0200

    add task solution

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import {
   useLocation,
   useParams,
 } from 'react-router-dom';
-import { Tabs, TabList, Tab, TabPanel } from 'react-tabs';
+import { Tabs, TabList, Tab, TabPanel } from 'react-Tabs';
 import 'react-tabs/style/react-tabs.css';
 
 const HomePage = () => (
@@ -85,6 +85,7 @@ export const App = () => {
   const location = useLocation();
   const isTabsActive = location.pathname.startsWith('/tabs');
   const isHomeActive = location.pathname === '/';
+
   return (
     <>
       <nav className="navbar is-light" data-cy="Nav">
